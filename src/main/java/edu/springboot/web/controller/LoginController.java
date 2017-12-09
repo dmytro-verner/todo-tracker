0a45ce6063570c4d741268025ded939dc7cb01c1
Dmitryi <dmytro.verner@gmail.com>
2017-12-09 15:05:12 +0200

    added basic project structure

---
+++ src/main/java/edu/springboot/web/controller/LoginController.java
@@ -0,0 +1,16 @@
+package edu.springboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseBody
+    public String loginMessage(){
+        return "Hello World";
+    }
+}
---
+++ src/test/java/edu/springboot/web/todotracker/TodoTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.springboot.web.todotracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
